1e002051bb574fef27331f6d33cfe7fe3ed4f74d
Alex Moon <alex.jared.moon@gmail.com>
2021-09-17 10:58:43 -0700

    fix: build script

--- atlas-build.mjs
+++ atlas-build.mjs
@@ -1,20 +1,17 @@
+import { exec } from "child_process";
 
-import { exec } from 'child_process';
+const ATLAS_ENV = process.env?.ATLAS_ENV || "production";
 
-const ATLAS_ENV = process.env?.ATLAS_ENV || 'production';
-
-console.log('ATLAS_ENV', ATLAS_ENV);
+console.log("ATLAS_ENV", ATLAS_ENV);
 
 let child;
 
-if(ATLAS_ENV === 'development') {
-  console.log('Running in development mode nothing to build');
+if (ATLAS_ENV === "development") {
+  console.log("Running in development mode nothing to build");
 } else {
-  console.log('Running in production, building Gatsby site.');
-  child = exec('npm run build', []);
+  console.log("Running in production, building Gatsby site.");
+  child = exec("npm run build", []);
+
+  child.stdout.pipe(process.stdout);
+  child.stderr.pipe(process.stderr);
 }
-
-child.stdout.pipe(process.stdout);
-child.stderr.pipe(process.stderr);
-
-
