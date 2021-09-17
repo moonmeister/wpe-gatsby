4ff8310120b82129dbf9805d4fc3c89a0b7868a1
Alex Moon <alex.jared.moon@gmail.com>
2021-09-17 10:55:50 -0700

    fix build and add fsify to config

---
+++ atlas-build.mjs
@@ -0,0 +1,20 @@
+
+import { exec } from 'child_process';
+
+const ATLAS_ENV = process.env?.ATLAS_ENV || 'production';
+
+console.log('ATLAS_ENV', ATLAS_ENV);
+
+let child;
+
+if(ATLAS_ENV === 'development') {
+  console.log('Running in development mode nothing to build');
+} else {
+  console.log('Running in production, building Gatsby site.');
+  child = exec('npm run build', []);
+}
+
+child.stdout.pipe(process.stdout);
+child.stderr.pipe(process.stderr);
+
+
--- gatsby-config.js
+++ gatsby-config.js
@@ -31,5 +31,6 @@ module.exports = {
       },
       __key: "images",
     },
+    "gatsby-plugin-fastify",
   ],
 };
